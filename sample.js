00fe07147ad092af2f33474da138a2d34bf28962
unknown <60006990@HSPWL9002PV.hospira.corp>
2015-02-27 00:53:27 +0530

    Submitting the NodeJs Worked entities [Two JS Files]

---
+++ sample.js
@@ -0,0 +1,4 @@
+var a=1;
+console.log(a +'There');
+
+module.exports.b =a;
---
+++ sample2.js
@@ -0,0 +1,7 @@
+var sample = require('./sample.js');
+var underscore = require('C:/Users/60006990/AppData/Roaming/npm/node_modules/underscore');
+
+console.log(underscore);
+console.log(sample.b);
+
+
